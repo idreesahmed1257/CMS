3f748dee243b66a3a2d0b2d5484a68e1b224b916
MLuqman150 <muhammadluqman12080@outlook.com>
2022-02-02 02:02:07 +0500

    Changed Again

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port =  process.env.PORT || 5000;
 app.use(express.static(__dirname +'/public'));
 
 // Routes
-
 //Rendering the login page
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/Student/first.html'));
