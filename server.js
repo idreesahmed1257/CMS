97994652155949c2d2b2129cfabab3459b080cc8
MLuqman150 <muhammadluqman12080@outlook.com>
2022-02-02 01:57:20 +0500

    Changed by Luqman

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port =  process.env.PORT || 5000;
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/Student/first.html'));
+});
 
 // Server listening on the port
 app.listen(port,()=>{
